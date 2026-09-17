aad1ab8a40f85ce65a218905287919ebe27885a0
DentalEmr_Densmart <developer@densoftinfotech.in>
2026-09-17 16:00:50 +0530

    Update CORS policy to allow Vercel domain

--- VehicleServiceTracker.Api/Program.cs
+++ VehicleServiceTracker.Api/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000")
+            .WithOrigins("https://vehicle-service-tracker-gamma.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
